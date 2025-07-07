94a0a1e25875c4e66e069bb9c9d1178170c6066f
andre-tech-id <a.nogueira.ideal@gmail.com>
2025-07-06 21:38:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let botao = document.querySelector("#botao");
+let mensagem = document.querySelector(".mensagem");
+
+botao.addEventListener("click",e =>{
+    mensagem.innerHTML="Seja bem-vindo ao meu primeiro site, espero que goste!";
+});
+    
